a92e970afb6f7d86f8899e768de949a235715ecc
Denisse Lemos <denu.a.lemos@gmail.com>
2021-05-19 20:49:52 -0300

    Avances

---
+++ OperacionesAritmeticas/main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+  x := 42
+  y := 8
+      
+  // Addition
+  res := x + y
+  fmt.Println(res)
+      
+  // Subtraction
+  res = x - y
+  fmt.Println(res)
+      
+  // Multiplication
+  res = x * y
+  fmt.Println(res)
+      
+  // Division
+  res = x / y
+  fmt.Println(res)
+      
+  // Modulus, results in the remainder of the division
+  res = x % y
+  fmt.Println(res) 
+}
